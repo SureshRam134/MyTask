69f45a070cf6396ca77a7e2b308d3afcbd28beb8
SureshRam134 <ss9477157@gmail.com>
2026-06-12 22:19:31 +0530

    today 12/06/2026 10 ques completed

---
+++ daily-task/frontend/src/task12/Students.jsx
@@ -0,0 +1,18 @@
+
+
+export const studentData = [
+    {
+        id: 1,
+        name: "Ravi",
+        skills: ["HTML", "CSS", "React"],
+        company: {
+            name: "TCS"
+        }
+    },
+    {
+        id: 2,
+        name: "Priya",
+        skills: ["JavaScript", "Node"],
+        company: {}
+    }
+]
